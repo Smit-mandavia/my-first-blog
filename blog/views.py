04c778804021ce970d3796afddce41da152303f3
Smit-Mandavia <patelsmitgreat@gmail.com>
2020-08-24 11:47:18 +0530

    added login required decorator

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,35 @@
-from django.shortcuts import render
+
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect
 from django.utils import timezone
-from  .models import Post
+from django.shortcuts import redirect
 from .forms import PostForm
-from django.contrib.auth.models import User
+from .models import Post
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
-    posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request,pk):
+
+def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html',{'post':post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required(login_url='/admin/login')
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            if isinstance(request.user,User) :
-                post.author = request.user
-            else :
-                post.author = User.objects.get(username='guest')
-            post.published_date=timezone.now()
+            post.author = request.user
+            post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail',pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request,'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required(login_url='/admin/login')
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
